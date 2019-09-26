0487888d94f46f1068e59c9d1f236733790c8e8b
gabyKOKAI <gaby@kokai.com.mx>
2019-09-26 08:28:48 -0500

    prueba imagen

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -41,7 +41,7 @@
                     VINT
                     <br-->
                     <img src="{{URL::asset('/images/vint.jpg')}}" class="img-responsive imgLogo2" title="Vint">
-                   <!--img src='http://vint.mx/images/vint_blanco.png?crc=31914949' alt='Vint Logo' class="center"-->
+                    <img src='http://vint.mx/images/vint_blanco.png?crc=31914949' alt='Vint Logo' class="center imgLogo2">
                 </div>
                 <div class="col-md-3">
                     @if (Auth::check())
