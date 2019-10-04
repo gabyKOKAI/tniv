2739f9e8c23f8affd28af1dacf238a4288f2476c
gabyKOKAI <gaby@kokai.com.mx>
2019-10-03 23:58:57 -0500

    PROBANDO MENU

--- public/js/vint.js
+++ public/js/vint.js
@@ -1,7 +1,9 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "20";
+  //document.getElementById("mySidenav").style.width = "20";
+  document.getElementById("mySidenav").style.display = "block";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "5";
+  //document.getElementById("mySidenav").style.width = "5";
+  document.getElementById("mySidenav").style.display = "none";
 }
