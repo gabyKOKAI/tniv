736dfa17a975189f31745c8af1251492c73c7f5c
gabyKOKAI <gaby@kokai.com.mx>
2019-10-04 15:54:48 -0500

    using newer jquery version

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="{{URL::asset('/js/vint.js')}}"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script>
+    <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!--script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script-->
     <script src="{{URL::asset('/css/Bootstrap/js/bootstrap.min.js')}}"></script>
 
