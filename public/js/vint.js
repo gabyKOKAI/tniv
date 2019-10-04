779dbb2581d7f7db87e38fc16f47676843ca2916
gabyKOKAI <gaby@kokai.com.mx>
2019-10-04 00:49:59 -0500

    menu

--- public/js/vint.js
+++ public/js/vint.js
@@ -1,11 +1,11 @@
 function openNav() {
   //document.getElementById("mySidenav").style.width = "20";
-  document.getElementById("mySidenav").style.display = "block";
-  alert("Hello! I am an alert box!!");
+  document.getElementById("mySidenav").style.display = "none";
+  //alert("Hello! I am an alert box!!");block
 }
 
 function closeNav() {
   //document.getElementById("mySidenav").style.width = "5";
   document.getElementById("mySidenav").style.display = "none";
-  alert("Hello! I am an alert box2222!!");
+  //alert("Hello! I am an alert box2222!!");
 }
